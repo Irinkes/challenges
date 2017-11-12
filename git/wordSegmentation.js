c96baff808ed934dd5c7a8dfb57a2aa0ac032a9d
Irinkes <irinkes@gmail.com>
2017-11-12 19:18:43 +0300

    added ambigious

--- git/wordSegmentation.js
+++ git/wordSegmentation.js
@@ -1,70 +1,76 @@
 function segmentify(dictionary, inputString) {
+    var currentWord;
+
+    for (var j = 0;j<dictionary.length;j++) {
+        currentWord = dictionary[j];
+
+        for (var k = 0;k<dictionary.length;k++) {
+            if (j !== k) {
+              if(currentWord.includes(dictionary[k]) && currentWord!==dictionary[k]){
+                  currentWord = currentWord.replace(dictionary[k],' ');
+              }
+            }
+        }
+        currentWord = currentWord.trim();
+        if(currentWord.length===0) {
+            return "AMBIGUOUS";
+        }
+    }
+
     var words = [];
 
     dictionary = dictionary.sort(function(a, b){
         return b.length - a.length;
     })
-    console.log(dictionary);
+
     var re;
+    var spaces;
     for(var i=0; i<dictionary.length; i++) {
         if(inputString.includes(dictionary[i])) {
-            if(words[inputString.indexOf(dictionary[i])]===undefined) {
-                words[inputString.indexOf(dictionary[i])] = dictionary[i];
+            if(inputString.split(dictionary[i]).length-1>1) {
+
+                var pos = 0;
+
+                while (true) {
+                    var foundPos = inputString.indexOf(dictionary[i], pos);
+                    if (foundPos == -1) break;
+
+                    words[foundPos] = dictionary[i];
+                    pos = foundPos + 1; // продолжить поиск со следующей
+                }
                 re = new RegExp(dictionary[i], 'g');
-                inputString = inputString.replace(re,' ');
+                spaces = ' '.repeat(dictionary[i].length);
+                inputString = inputString.replace(re,spaces);
+
             }
-            else if(words[inputString.indexOf(dictionary[i])]!==undefined && words[inputString.indexOf(dictionary[i])]!==dictionary[i]){
-               return "AMBIGUOUS";
+            else {
+                if(words[inputString.indexOf(dictionary[i])]===undefined) {
+                    words[inputString.indexOf(dictionary[i])] = dictionary[i];
+                    spaces = ' '.repeat(dictionary[i].length);
+                    inputString = inputString.replace(dictionary[i],spaces);
+                }
+                else if(words[inputString.indexOf(dictionary[i])]!==undefined && words[inputString.indexOf(dictionary[i])]!==dictionary[i]){
+                    return "AMBIGUOUS";
+                }
             }
-
-        }
-    }
-    console.log(inputString);
-    console.log(words);
-    function removeEmpties(item){
-        return item;
-    }
-    words = words.filter(removeEmpties);
-    var trimmedString;
-
-    /*Функция для проверки совпадения хотя бы одного элемента в массиве со строкой */
-    function checkString(item){
-        if(trimmedString.includes(item)) {
-            return item;
         }
     }
 
-    /*укорачиваем строчку на найденные в словаре слова, чтобы найти в ней слова, которых нет в словаре */
-    function stringTrimming(str) {
-        for(i=0;i<words.length;i++) {
-            str = str.replace(words[i], '');
-        }
-        return str;
+    inputString = inputString.trim();
+    if(inputString.length>0) {
+        return "IMPOSSIBLE";
     }
-
-    trimmedString = stringTrimming(inputString);
-    console.log('trimmedString: '+ trimmedString);
-
-
-    while(trimmedString.length>0) {
-        if(words.some(checkString)) {
-            stringTrimming(trimmedString);
-            trimmedString = stringTrimming(trimmedString);
-            // console.log('trimmedString inside function: '+trimmedString);
-        }
-        else {
-            return "IMPOSSIBLE";
-        }
-    }
-
-    if(inputString.length===0 || trimmedString.length===0) {
+    else {
+        words = words.filter(removeEmpties);
         for(i=0;i<words.length-1;i++) {
             words[i]=words[i]+' ';
         }
-
         return words.join('');
     }
 
+    function removeEmpties(item){
+        return item;
+    }
 
 }
 const dictionary = ['the', 'chicken', 'crossed', 'road'];
@@ -72,4 +78,7 @@ const inputString = "thechickencrossedtheroad";
 
 // const dictionary = ["weather", "the", "how", "is"];
 // const inputString = "howistheweatherhow";
+
+// const dictionary = ['examples', 'over', 'haul', 'overhaul', 'the'];
+// const inputString = "overhaultheexamples";
 console.log(segmentify(dictionary, inputString));
